5b087dcfbb37eaab76fa1a24dc3e1afc670524a6
mahdiar <kaheh.1384@gmail.com>
2025-01-03 17:05:08 +0330

    chore: add tutorial intro modal

--- app.vue
+++ app.vue
@@ -1,29 +1,14 @@
 <template>
-  <div class="w-96 mx-auto h-dvh flex flex-col justify-between">
+  <div class="lg:w-96 w-full lg:px-0 px-6 mx-auto h-dvh">
+    <FeatureIntroTutorial></FeatureIntroTutorial>
+
     <LayoutHeader></LayoutHeader>
 
     <nuxt-page/>
 
-    <!-- <BaseModal v-model="isOpen" @close="isOpen = false">
-      <template #header>
-        <img src="@/assets/gif/tutorial.gif" alt="game tutorial" loading="lazy" class="w-full h-32 object-cover rounded-2xl border border-gray-200 mb-4">
-      </template>
-      <template #body>
-        <h3 class="font-bold text-lg">
-          سلام، به بازی خوش اومدی
-        </h3>
-        <p class="mt-1">
-          کارت های یکسان رو پیدا کن و انتخابشون کن تا برنده بشی
-        </p>
-      </template>
-    </BaseModal>
-    
-    <button @click="isOpen = true">open </button> -->
     <BaseSplashScreen></BaseSplashScreen>
-    <LayoutFooter></LayoutFooter>
   </div>
 </template>
 <script setup lang="ts">
 
-const isOpen = ref(false)
 </script>
---
+++ components/feature/IntroTutorial.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const introCookie = useCookie('DisableTutorial')
+const showIntroModal = ref<boolean>(!introCookie.value)
+function closeIntroModal() :void {
+  showIntroModal.value = false;
+  introCookie.value = '1';
+}
+</script>
+
+<template>
+  <BaseModal v-model="showIntroModal" @close="closeIntroModal">
+    <template #body>
+      <img src="@/assets/gif/tutorial.gif" alt="game tutorial" loading="lazy" class="w-full h-36 object-cover rounded-2xl border-2 border-gray-200 mb-4 grayscale">
+      <h3 class="font-bold text-lg">
+        سلام، به بازی خوش اومدی
+      </h3>
+      <p class="mt-1">
+        کارت های یکسان رو پیدا کن و انتخابشون کن تا امتیاز بگیری
+      </p>
+    </template>
+    <template #footer>
+      <BaseButton class="w-full mt-10" @click="closeIntroModal">
+        شروع بازی!
+      </BaseButton>
+    </template>
+  </BaseModal>
+</template>
+
+<style scoped>
+
+</style>
